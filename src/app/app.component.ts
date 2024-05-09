d1af60676edfe4cb0e04c2c7a65c5bdeb7648c9e
g1ann0z <giannozcydia@gmail.com>
2024-05-09 15:20:42 +0200

    startTypingChallengeApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker'; 
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'typing';
+
+  randomText = lorem.sentence();
+
+
 }
