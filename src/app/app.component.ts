37b1984591d46b19c698cd2f3e4ccde8ca369513
g1ann0z <giannozcydia@gmail.com>
2024-05-12 18:32:30 +0200

    end_typing_challeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { faker } from '@faker-js/faker'; 
 import { lorem } from 'faker';
+import { parseTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,19 @@ import { lorem } from 'faker';
 export class AppComponent {
 
   randomText = lorem.sentence();
+  userText = "";
+
+  onInput(value: string){
+    /* console.log(value); */
+    this.userText = value;
+  }
+
+  compare(randomLetter: string, userLetter: string){
+    if(!userLetter){
+      return 'pending';
+    }
+    return userLetter === randomLetter ? 'correct' : 'incorrect';
+  }
 
 
 }
